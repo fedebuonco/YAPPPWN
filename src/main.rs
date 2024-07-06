986dd272c432dad6a28060437d4082073112b27b
fedebuonco <federico.buonco@gmail.com>
2024-07-06 22:30:49 +0100

    Pnet first commit

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,30 @@ use pcap::{Active, Capture, Device};
 
 use crate::exploit::Exploit;
 
-fn run_exploit(active_capture: Capture<Active>) {
-    // Exploit
-    let mut exploit = Exploit {
-        target_mac: [0, 0, 0, 0, 0, 0],
-        pppoe_softc: 0,
-        source_mac: [0, 0, 0, 0, 0, 0],
-        host_uniq: [0, 0, 0, 0, 0, 0, 0, 0],
-    };
-    // PPP negotiation
-    exploit.ppp_negotiation(active_capture);
-}
+// fn run_exploit(active_capture: Capture<Active>) {
+//     // Exploit
+//     let mut exploit = Exploit {
+//         target_mac: [0, 0, 0, 0, 0, 0],
+//         pppoe_softc: 0,
+//         source_mac: [0, 0, 0, 0, 0, 0],
+//         host_uniq: [0, 0, 0, 0, 0, 0, 0, 0],
+//     };
+//     // PPP negotiation
+//     exploit.ppp_negotiation(active_capture);
+// }
 
 fn main() {
-    // Cap device
-    let interface = "en10";
-    // Open the specified interface
-    let dev = Device::list()
-        .unwrap()
-        .into_iter()
-        .find(|d| d.name == interface)
-        .expect("Specified interface not found");
-    let cap = Capture::from_device(dev).unwrap();
+    // // Cap device
+    // let interface = "en10";
+    // // Open the specified interface
+    // let dev = Device::list()
+    //     .unwrap()
+    //     .into_iter()
+    //     .find(|d| d.name == interface)
+    //     .expect("Specified interface not found");
+    // let cap = Capture::from_device(dev).unwrap();
 
-    let open_cap = cap.timeout(10000).open().unwrap();
+    // let open_cap = cap.timeout(10000).open().unwrap();
 
-    run_exploit(open_cap)
+    // run_exploit(open_cap)
 }
