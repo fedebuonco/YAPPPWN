fd9231c77e30003c0135be3f45fd71621e2ce6e9
Fedeb <federico.buonco@gmail.com>
2024-07-06 23:43:55 +0100

    PPP Negotiation works until pado creation

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,33 @@
 mod constants;
 mod exploit;
 use pcap::{Active, Capture, Device};
+use pnet::datalink::{self, NetworkInterface};
 
 use crate::exploit::Exploit;
 
-// fn run_exploit(active_capture: Capture<Active>) {
-//     // Exploit
-//     let mut exploit = Exploit {
-//         target_mac: [0, 0, 0, 0, 0, 0],
-//         pppoe_softc: 0,
-//         source_mac: [0, 0, 0, 0, 0, 0],
-//         host_uniq: [0, 0, 0, 0, 0, 0, 0, 0],
-//     };
-//     // PPP negotiation
-//     exploit.ppp_negotiation(active_capture);
-// }
+fn run_exploit() {
+    // Find interface
+    let interface_name = "enp4s0";
+    let interface_names_match = |iface: &NetworkInterface| iface.name == interface_name;
+
+    // Find the network interface with the provided name
+    let interfaces = datalink::interfaces();
+    let interface = interfaces
+        .into_iter()
+        .filter(interface_names_match)
+        .next()
+        .unwrap();
+    // Exploit
+    let mut exploit = Exploit {
+        target_mac: [0, 0, 0, 0, 0, 0],
+        pppoe_softc: 0,
+        source_mac: [0, 0, 0, 0, 0, 0],
+        host_uniq: [0, 0, 0, 0, 0, 0, 0, 0],
+    };
+    // PPP negotiation
+    exploit.ppp_negotiation(interface);
+}
 
 fn main() {
-    // // Cap device
-    // let interface = "en10";
-    // // Open the specified interface
-    // let dev = Device::list()
-    //     .unwrap()
-    //     .into_iter()
-    //     .find(|d| d.name == interface)
-    //     .expect("Specified interface not found");
-    // let cap = Capture::from_device(dev).unwrap();
-
-    // let open_cap = cap.timeout(10000).open().unwrap();
-
-    // run_exploit(open_cap)
+    run_exploit()
 }
