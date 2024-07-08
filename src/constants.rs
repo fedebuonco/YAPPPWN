087e3f51d879a795c9b74deb63469235bdebf0cb
FedeBuonco <34037265+fedebuonco@users.noreply.github.com>
2024-07-08 23:49:46 +0100

    Switch to Pnet for sending packets (#3)
    * Pnet first commit
    
    * PPP Negotiation works until pado creation
    
    * Create listen for packet
    
    * PPP Negotiation Done
    
    * Add pads test

--- src/constants.rs
+++ src/constants.rs
@@ -2,9 +2,11 @@
 
 pub const ETHERTYPE_PPPOEDISC: u16 = 0x8863;
 pub const PPPOE_CODE_PADI: u8 = 0x09;
+pub const PPPOE_CODE_PADR: u8 = 0x19;
 pub const PPPOE_CODE_PADO: u8 = 0x07;
+pub const PPPOE_CODE_PADS: u8 = 0x65;
 pub const PPPOE_SOFTC_SC_AC_COOKIE: u8 = 0x40;
-
+pub const PPPOE_SESSION_ID: u16 = 0xffff;
 pub const ETH_HEADER_LEN: usize = 14;
 pub const ETH_SOURCE_MAC: usize = 6;
 pub const PPPOE_HEADER_LEN: usize = 6;
